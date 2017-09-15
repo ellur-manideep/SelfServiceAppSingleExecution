63acdea8f1cac9a511b6d191c6d9865cfad56386
Manideep Ellur <ellurm@juniper.net>
2017-09-15 16:10:15 +0530

    working fine# Please enter the commit message for your changes. Lines starting

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -34,33 +34,33 @@ sapApp.controller('SapCtrl', ['$scope', '$timeout', '$mdSidenav', '$log', '$http
   $scope.myFile = [];
   var i = 1;
   $scope.jenkinBuild = function(){
-     console.log($scope.testData.length);
-      console.log($scope.myFile[i]);
-      var file = $scope.myFile[i];
-      var uploadUrl = "/multer";
-      var fd = new FormData();
-      fd.append('file', file);
+    console.log($scope.testData.length);
+    console.log($scope.myFile[i]);
+    var file = $scope.myFile[i];
+    var uploadUrl = "/multer";
+    var fd = new FormData();
+    fd.append('file', file);
 
-      $http({
-        method: 'POST',
-        url: uploadUrl,
-        data: fd,
-        transformRequest: angular.identity,
-        headers: {'Content-Type': undefined}
-      })
-      .then(function(response){
-        console.log(response.data);
-        $scope.uploaded[i]=true;
-        if(i<=$scope.testData.length){
-          i = i + 1;
-          $scope.jenkinBuild();
-        }
-        console.log("done");
-        
-        //$scope.i = $scope.i + 1;
-      });
+    $http({
+      method: 'POST',
+      url: uploadUrl,
+      data: fd,
+      transformRequest: angular.identity,
+      headers: {'Content-Type': undefined}
+    })
+    .then(function(response){
+      console.log(response.data);
+      $scope.uploaded[i]=true;
+      if(i<=$scope.testData.length){
+        i = i + 1;
+        $scope.jenkinBuild();
+      }
+      console.log("done");
 
-    };
+      //$scope.i = $scope.i + 1;
+    });
+
+  };
 
 
 
